c8d95a3dcdccedf31d2deaaa3ae44f2891353add
ibahmman <bahmansobhan@gmail.com>
2024-03-26 01:28:36 +0330

    import models

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Service, Product, ActionLogs, Location, Datacenter, PCloud, SCloud
 
-# Register your models here.
+
+admin.site.register(Service)
+admin.site.register(Product)
+admin.site.register(ActionLogs)
+admin.site.register(Location)
+admin.site.register(Datacenter)
+admin.site.register(PCloud)
+admin.site.register(SCloud)
